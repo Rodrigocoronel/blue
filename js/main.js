5b4bc697b1d97f965bef05afb7486ff93668a7e2
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2018-10-09 13:27:05 -0700

    blu

--- js/main.js
+++ js/main.js
@@ -11,8 +11,31 @@
     overflowScroll: true,
     updateHash: true,
     touchScroll:true,
-    before:function() {},
-    after:function() {},
+    before:function() {
+        
+    },
+    after:function() {
+        let liga = window.location.href
+        let char = liga.indexOf('#');
+        let ref = liga.substring(char);
+
+        if(ref == '#home')
+            $('.navbar').addClass('d-none');
+        else 
+            $('.navbar').removeClass('d-none');
+
+    },
     afterResize:function() {},
     afterRender:function() {}
   });
+
+$(function() {
+    $( ".btn" ).click(function() {
+       $.scrollify.move(this.name);
+    });
+});
+
+$(function(){
+    
+});
+
