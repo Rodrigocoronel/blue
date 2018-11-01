acff9dc8f07af5c63b8633907b48f4c2c96279e2
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2018-11-01 16:33:37 -0700

    cambios menu y mobile

--- js/main.js
+++ js/main.js
@@ -18,16 +18,51 @@
         let liga = window.location.href
         let char = liga.indexOf('#');
         let ref = liga.substring(char);
+        let element = null;
 
         if(ref == '#home')
             $('.navbar').addClass('d-none');
         else 
             $('.navbar').removeClass('d-none');
 
+        element1 = document.getElementById('navHome')
+        element2 = document.getElementById('navOrgullo')
+        element3 = document.getElementById('navHistoria')
+        element4 = document.getElementById('navNoticias')
+        element5 = document.getElementById('navContacto')
+
+        $(element1).removeClass('menuActual')
+        $(element2).removeClass('menuActual')
+        $(element3).removeClass('menuActual')
+        $(element4).removeClass('menuActual')
+        $(element5).removeClass('menuActual')
+
+        switch(ref){
+            case '#home':
+                $(element1).addClass('menuActual')
+                break;
+            case '#orgulloAzul':
+                $(element2).addClass('menuActual')
+                break;
+            case '#historia':
+                 $(element3).addClass('menuActual')
+                break;
+            case '#noticias':
+                $(element4).addClass('menuActual')
+                break;
+            case '#contacto':
+                $(element5).addClass('menuActual')
+                break;
+        }
+
     },
     afterResize:function() {},
     afterRender:function() {}
   });
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
 
 $(function() {
     $( ".btn" ).click(function() {
@@ -35,7 +70,12 @@ $(function() {
     });
 });
 
-$(function(){
-    
+$( document ).ready(function() {
+    if(isMobileDevice()){
+        $.scrollify.disable();
+    }else{
+        $.scrollify.enable();
+    }
 });
+
 
